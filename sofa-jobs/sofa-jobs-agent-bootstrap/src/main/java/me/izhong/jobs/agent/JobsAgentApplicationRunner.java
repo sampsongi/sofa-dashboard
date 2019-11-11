5c84f07cdb76279ad00b21b5221df5edda3cbd54
jjzhong <jjzhong@chianums.com>
2019-11-11 13:15:32 +0800

    去掉springboot部分自动配置

--- sofa-jobs/sofa-jobs-agent-bootstrap/src/main/java/me/izhong/jobs/agent/JobsAgentApplicationRunner.java
+++ sofa-jobs/sofa-jobs-agent-bootstrap/src/main/java/me/izhong/jobs/agent/JobsAgentApplicationRunner.java
@@ -8,14 +8,22 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ImportResource;
 
 import java.util.*;
 
 @ImportResource({ "classpath*:rpc-sofa-boot-starter.xml" })
-@SpringBootApplication
-@ComponentScan(value = {"com.chinaums,me.izhong"})
+@SpringBootApplication(exclude = {
+        DataSourceAutoConfiguration.class,
+        DataSourceTransactionManagerAutoConfiguration.class,
+        HibernateJpaAutoConfiguration.class,
+        MongoAutoConfiguration.class})
+@ComponentScan(value = {"com.xuis,me.izhong"})
 @Slf4j
 public class JobsAgentApplicationRunner implements ApplicationRunner {
     public static void main(String[] args) {
