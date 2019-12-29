d178a2ccbc379ddf7f3ef8c1d938b80b50135e9c
jimmy <jjzhong_mac@izhongme>
2019-12-29 13:37:00 +0800

    屏蔽一些要素

--- sofa-admin/sofa-admin-bootstrap/src/main/java/me/izhong/dashboard/web/service/DictService.java
+++ sofa-admin/sofa-admin-bootstrap/src/main/java/me/izhong/dashboard/web/service/DictService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service("dict")
 public class DictService {
@@ -14,7 +15,16 @@ public class DictService {
     private SysDictDataService sysDictDataService;
 
     public List<SysDictData> getType(String dictType) {
-        return sysDictDataService.selectNormalDictDataByType(dictType);
+        List<SysDictData> lists = sysDictDataService.selectNormalDictDataByType(dictType);
+        lists = lists.stream().map(e->{
+            e.setId(null);
+            e.setCreateTime(null);
+            e.setUpdateTime(null);
+            e.setCreateBy(null);
+            e.setUpdateBy(null);
+            return e;
+        }).collect(Collectors.toList());
+        return lists;
     }
 
     public String getLabel(String dictType, String dictValue) {
