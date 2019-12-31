ab91e0b77c0717c4c295edfc086cf89433023456
jjzhong <jjzhong@chianums.com>
2019-12-31 09:43:48 +0800

    增加一些注释

--- sofa-jobs/sofa-jobs-agent-bootstrap/src/main/java/me/izhong/jobs/agent/util/MailUtil.java
+++ sofa-jobs/sofa-jobs-agent-bootstrap/src/main/java/me/izhong/jobs/agent/util/MailUtil.java
@@ -16,21 +16,77 @@ import java.util.Properties;
 @Slf4j
 public class MailUtil {
 
+    /**
+     * 发送邮件，无附件
+     * @param host 邮件服务器主机，ip地址或者域名
+     * @param port 端口 smtp默认是25 ssl加密默认是465
+     * @param senderAccount 发送方的账号
+     * @param senderPassword 发送方的密码
+     * @param senderAddress 发送方的邮件地址
+     * @param senderName 发送方的名称
+     * @param recipientAddress 接收方的邮件地址，多个用,分割
+     * @param title 邮件的标题
+     * @param body 邮件的内容
+     * @throws Exception
+     */
     public static void sendMail(String host, int port, String senderAccount, String senderPassword, String senderAddress, String senderName,
                                 String recipientAddress, String title, String body) throws Exception {
         sendMail(host, port, senderAccount, senderPassword, senderAddress, senderName, recipientAddress, title, body, null, null);
     }
 
+    /**
+     * 发送邮件，有附件
+     * @param host 邮件服务器主机，ip地址或者域名
+     * @param port 端口 smtp默认是25 ssl加密默认是465
+     * @param senderAccount 发送方的账号
+     * @param senderPassword 发送方的密码
+     * @param senderAddress 发送方的邮件地址
+     * @param senderName 发送方的名称
+     * @param recipientAddress 接收方的邮件地址，多个用,分割
+     * @param title 邮件的标题
+     * @param body 邮件的内容
+     * @param fileFullName 文件的绝对路径名称 字符格式
+     * @throws Exception
+     */
     public static void sendMail(String host, int port, String senderAccount, String senderPassword, String senderAddress, String senderName,
                                 String recipientAddress, String title, String body, String fileFullName) throws Exception {
         sendMail(host, port, senderAccount, senderPassword, senderAddress, senderName, recipientAddress, title, body, new File(fileFullName), new File(fileFullName).getName());
     }
 
+    /**
+     * 发送邮件 有附件
+     * @param host 邮件服务器主机，ip地址或者域名
+     * @param port 端口 smtp默认是25 ssl加密默认是465
+     * @param senderAccount 发送方的账号
+     * @param senderPassword 发送方的密码
+     * @param senderAddress 发送方的邮件地址
+     * @param senderName 发送方的名称
+     * @param recipientAddress 接收方的邮件地址，多个用,分割
+     * @param title 邮件的标题
+     * @param body 邮件的内容
+     * @param file 文件对象
+     * @throws Exception
+     */
     public static void sendMail(String host, int port, String senderAccount, String senderPassword, String senderAddress, String senderName,
                                 String recipientAddress, String title, String body, File file) throws Exception {
         sendMail(host, port, senderAccount, senderPassword, senderAddress, senderName, recipientAddress, title, body, file, file.getName());
     }
 
+    /**
+     * 发送邮件，有附件
+     * @param host 邮件服务器主机，ip地址或者域名
+     * @param port 端口 smtp默认是25 ssl加密默认是465
+     * @param senderAccount 发送方的账号
+     * @param senderPassword 发送方的密码
+     * @param senderAddress 发送方的邮件地址
+     * @param senderName 发送方的名称
+     * @param recipientAddress 接收方的邮件地址，多个用,分割
+     * @param title 邮件的标题
+     * @param body 邮件的内容
+     * @param file 文件对象
+     * @param fileName 文件名称，接收方在邮件中看到的文件名称
+     * @throws Exception
+     */
     public static void sendMail(String host, int port, String senderAccount, String senderPassword, String senderAddress, String senderName,
                                 String recipientAddress, String title, String body, File file, String fileName) throws Exception {
         Properties props = new Properties();
