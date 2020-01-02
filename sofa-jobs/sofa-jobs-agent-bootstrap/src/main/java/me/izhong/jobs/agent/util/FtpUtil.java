73fad47ca7582e9917398eef343915c4602bb0fc
jjzhong <jjzhong@chianums.com>
2020-01-02 14:30:13 +0800

    优化下ftputil工具类

--- sofa-jobs/sofa-jobs-agent-bootstrap/src/main/java/me/izhong/jobs/agent/util/FtpUtil.java
+++ sofa-jobs/sofa-jobs-agent-bootstrap/src/main/java/me/izhong/jobs/agent/util/FtpUtil.java
@@ -23,81 +23,75 @@ public class FtpUtil {
 	/**
 	 * 发送文件到ftp服务器
 	 * 
-	 * @param host
-	 *            主机名，格式为<ftp|sftp>://hostname:port
-	 * @param user
-	 *            用户名
-	 * @param pass
-	 *            密码
-	 * @param destDir
-	 *            文件上传目录
-	 * @param destFile
-	 *            文件名
-	 * @param srcFile
-	 *            源文件
+	 * @param host 主机名，格式为<ftp|sftp>://hostname:port
+	 * @param user 用户名
+	 * @param pass 密码
+	 * @param ftpDir 文件上传目录 如果不存在会自动创建
+	 * @param ftpFile 文件名
+	 * @param srcFile 源文件
 	 * @throws Exception
 	 */
 	public static void putFileToFtp(String host, String user, String pass,
-			String destDir, String destFile, File srcFile) throws Exception {
+			String ftpDir, String ftpFile, File srcFile) throws Exception {
 		checkAddress(host,user,pass);
 		if(srcFile == null || !srcFile.exists())
 			throw new Exception("srcFile为空,或者不存在");
 		String srcName = srcFile.getName();
-		String sep = (StringUtils.isBlank(destDir)||destDir.endsWith("/")) ? "" : "/";
-		log.info("上传文件 {} 到 {}{}{}",srcName,destDir,sep,destFile);
+		String sep = (StringUtils.isBlank(ftpDir)||ftpDir.endsWith("/")) ? "" : "/";
+		log.info("上传文件 {} 到 {}{}{}",srcName,ftpDir,sep,ftpFile);
 		if(srcFile == null || !srcFile.exists())
 			throw new Exception("srcFile为空,或者不存在");
 		if (host.startsWith("sftp")) {
-			putSftp(host, user, pass, destDir, destFile, srcFile);
+			putSftp(host, user, pass, ftpDir, ftpFile, srcFile);
 		} else if (host.startsWith("ftp")) {
-			putFtp(host, user, pass, destDir, destFile, srcFile);
+			putFtp(host, user, pass, ftpDir, ftpFile, srcFile);
 		}
 	}
 
 	public static void getFileFromFtp(String host, String user, String pass,
-			String srcDir, String srcFile, File destFile) throws Exception {
+			String ftpDir, String ftpFile, File destFile) throws Exception {
 		checkAddress(host,user,pass);
 		if(destFile == null || !destFile.exists())
 			throw new Exception("destFile为空,或者不存在");
 		if (host.startsWith("sftp")) {
-			getSftp(host, user, pass, srcDir, srcFile, destFile);
+			getSftp(host, user, pass, ftpDir, ftpFile, destFile);
 		} else if (host.startsWith("ftp")) {
-			getFtp(host, user, pass, srcDir, srcFile, destFile);
+			getFtp(host, user, pass, ftpDir, ftpFile, destFile);
 		}
 	}
 
 	public static FileInfo getFileInfoFromFtp(String host, String user, String pass,
-											  String srcDir, String srcFile) throws Exception{
+											  String ftpDir, String ftpFile) throws Exception{
 		checkAddress(host,user,pass);
 		if (host.startsWith("sftp")) {
-			return getSftpFileInfo(host, user, pass, srcDir, srcFile);
+			return getSftpFileInfo(host, user, pass, ftpDir, ftpFile);
 		} else if (host.startsWith("ftp")) {
-			return getFtpFileInfo(host, user, pass, srcDir, srcFile);
+			return getFtpFileInfo(host, user, pass, ftpDir, ftpFile);
 		}
 		return null;
 	}
 
 	public static List<String> listFtpFilesInDir(String host, String user,
-			String pass, String destDir) throws Exception {
+			String pass, String ftpDir) throws Exception {
 		checkAddress(host,user,pass);
 		if (host.startsWith("sftp")) {
-			return listSftpFiles(host, user, pass, destDir);
+			return listSftpFiles(host, user, pass, ftpDir);
 		} else if (host.startsWith("ftp")) {
-			return listFtpFiles(host, user, pass, destDir);
+			return listFtpFiles(host, user, pass, ftpDir);
 		} else {
 			throw new Exception("FTP地址异常");
 		}
 	}
 
 	public static void deleteFileFromFtp(String host, String user, String pass,
-									String destDir, String destFile) throws Exception {
-		if(StringUtils.isBlank(destFile))
-			throw new Exception("destFile不能为空");
+									String ftpDir, String ftpFile) throws Exception {
+		if(StringUtils.isBlank(ftpFile))
+			throw new Exception("ftpFile不能为空");
 		checkAddress(host,user,pass);
 		if (host.startsWith("sftp")) {
-			deleteSftp(host, user, pass, destDir, destFile);
+			deleteSftp(host, user, pass, ftpDir, ftpFile);
 		} else if (host.startsWith("ftp")) {
-			deleteFtp(host, user, pass, destDir, destFile);
+			deleteFtp(host, user, pass, ftpDir, ftpFile);
 		}
 	}
 
@@ -125,7 +119,7 @@ public class FtpUtil {
 	}
 
 	private static void getFtp(String host, String user, String pass,
-			String srcDir, String srcFile, File destFile) throws Exception {
+			String ftpDir, String ftpFile, File destFile) throws Exception {
 		String s1[] = host.split("://");
 		String s2[] = s1[1].split(":");
 
@@ -145,8 +139,10 @@ public class FtpUtil {
 
 		ftp.setFileType(FTP.BINARY_FILE_TYPE);
 		ftp.setBufferSize(1024);
-		log.info("cd");
-		ftp.changeWorkingDirectory(srcDir);
+		if(StringUtils.isNotBlank(ftpDir)) {
+			log.info("cd");
+			ftp.changeWorkingDirectory(ftpDir);
+		}
 
 		// 设置被动模式
 		ftp.enterLocalPassiveMode();
@@ -154,7 +150,7 @@ public class FtpUtil {
 		@Cleanup
 		FileOutputStream fos = new FileOutputStream(destFile);
 		log.info("get");
-		boolean result = ftp.retrieveFile(srcFile, fos);
+		boolean result = ftp.retrieveFile(ftpFile, fos);
 
 		log.info("disconnect");
 		ftp.disconnect();
@@ -164,7 +160,7 @@ public class FtpUtil {
 	}
 
 	private static void getSftp(String host, String user, String pass,
-			String srcDir, String srcFile, File destFile) throws Exception {
+			String ftpDir, String ftpFile, File destFile) throws Exception {
 		String s1[] = host.split("://");
 		String s2[] = s1[1].split(":");
 
@@ -184,12 +180,14 @@ public class FtpUtil {
 		channel.connect();
 		ChannelSftp c = (ChannelSftp) channel;
 
-		log.info("cd");
-		c.cd(srcDir);
+		if(StringUtils.isNotBlank(ftpDir)) {
+			log.info("cd");
+			c.cd(ftpDir);
+		}
 		@Cleanup
 		FileOutputStream fos = new FileOutputStream(destFile);
 		log.info("get");
-		c.get(srcFile, fos);
+		c.get(ftpFile, fos);
 
 		log.info("disconnect");
 		c.disconnect();
@@ -197,7 +195,7 @@ public class FtpUtil {
 	}
 
 
-	private static FileInfo getSftpFileInfo(String host, String user, String pass, String srcDir, String srcFile)  throws Exception{
+	private static FileInfo getSftpFileInfo(String host, String user, String pass, String ftpDir, String ftpFile)  throws Exception{
 		String s1[] = host.split("://");
 		String s2[] = s1[1].split(":");
 
@@ -217,14 +215,16 @@ public class FtpUtil {
 		channel.connect();
 		ChannelSftp c = (ChannelSftp) channel;
 
-		log.info("cd");
-		c.cd(srcDir);
+		if(StringUtils.isNotBlank(ftpDir)) {
+			log.info("cd");
+			c.cd(ftpDir);
+		}
 		log.info("get");
-		SftpATTRS sts = c.stat(srcFile);
+		SftpATTRS sts = c.stat(ftpFile);
 		if(sts == null)
 			return null;
 		FileInfo fileInfo = new FileInfo();
-		fileInfo.setFileName(srcFile);
+		fileInfo.setFileName(ftpFile);
 		Date var1 = new Date((long)sts.getMTime() * 1000L);
 		fileInfo.setModifyTime(var1);
 		fileInfo.setSize(sts.getSize());
@@ -235,7 +235,7 @@ public class FtpUtil {
 		return fileInfo;
 	}
 
-	private static FileInfo getFtpFileInfo(String host, String user, String pass, String srcDir, String srcFile)  throws Exception{
+	private static FileInfo getFtpFileInfo(String host, String user, String pass, String ftpDir, String ftpFile)  throws Exception{
 		String s1[] = host.split("://");
 		String s2[] = s1[1].split(":");
 
@@ -255,24 +255,26 @@ public class FtpUtil {
 
 		ftp.setFileType(FTP.BINARY_FILE_TYPE);
 		ftp.setBufferSize(1024);
-		log.info("cd");
-		ftp.changeWorkingDirectory(srcDir);
+		if(StringUtils.isNotBlank(ftpDir)) {
+			log.info("cd");
+			ftp.changeWorkingDirectory(ftpDir);
+		}
 
 		// 设置被动模式
 		ftp.enterLocalPassiveMode();
 
 		log.info("get");
-		FTPFile[] ftpFiles = ftp.listFiles(srcFile);
+		FTPFile[] ftpFiles = ftp.listFiles(ftpFile);
 		if(ftpFiles == null || ftpFiles.length  == 0)
 			return null;
-		FTPFile ftpFile = ftpFiles[0];
+		FTPFile ff = ftpFiles[0];
 
-		Calendar calendar = ftpFile.getTimestamp();
+		Calendar calendar = ff.getTimestamp();
 		int zoneOffset = calendar.get(java.util.Calendar.ZONE_OFFSET);
 
 		FileInfo fileInfo = new FileInfo();
-		fileInfo.setFileName(srcFile);
-		String modifyString = ftp.getModificationTime(srcFile);
+		fileInfo.setFileName(ftpFile);
+		String modifyString = ftp.getModificationTime(ftpFile);
 		if(StringUtils.isBlank(modifyString))
 			return null;
 		String dateStr = modifyString.split(" ")[1];
@@ -280,7 +282,7 @@ public class FtpUtil {
 		calendar.set(Calendar.SECOND,date2.getSeconds());
 		Date date = DateUtils.addMilliseconds(calendar.getTime(),zoneOffset);
 		fileInfo.setModifyTime(date);
-		fileInfo.setSize(ftpFile.getSize());
+		fileInfo.setSize(ff.getSize());
 
 		log.info("disconnect");
 		ftp.disconnect();
@@ -479,7 +481,7 @@ public class FtpUtil {
 	}
 
 	private static void deleteFtp(String host, String user, String pass,
-							   String destDir, String destFile) throws Exception {
+							   String ftpDir, String ftpFile) throws Exception {
 		String s1[] = host.split("://");
 		String s2[] = s1[1].split(":");
 
@@ -498,16 +500,16 @@ public class FtpUtil {
 		}
 
 		ftp.setFileType(FTP.BINARY_FILE_TYPE);
-		if(StringUtils.isNotBlank(destDir)) {
-			log.info("cd:{}", destDir);
-			ftp.changeWorkingDirectory(destDir);
+		if(StringUtils.isNotBlank(ftpDir)) {
+			log.info("cd:{}", ftpDir);
+			ftp.changeWorkingDirectory(ftpDir);
 		}
 
 		// 设置被动模式
 		ftp.enterLocalPassiveMode();
 
-		log.info("delete:{}",destFile);
-		boolean result = ftp.deleteFile(destFile);
+		log.info("delete:{}",ftpFile);
+		boolean result = ftp.deleteFile(ftpFile);
 
 		log.info("disconnect");
 		ftp.disconnect();
@@ -517,7 +519,7 @@ public class FtpUtil {
 	}
 
 	private static void deleteSftp(String host, String user, String pass,
-								String destDir, String destFile) throws Exception {
+								String ftpDir, String ftpFile) throws Exception {
 		String s1[] = host.split("://");
 		String s2[] = s1[1].split(":");
 
@@ -537,12 +539,12 @@ public class FtpUtil {
 		channel.connect();
 		ChannelSftp c = (ChannelSftp) channel;
 
-		if(StringUtils.isNotBlank(destDir)) {
-			log.info("cd:{}", destDir);
-			c.cd(destDir);
+		if(StringUtils.isNotBlank(ftpDir)) {
+			log.info("cd:{}", ftpDir);
+			c.cd(ftpDir);
 		}
-		log.info("delete :{}",destFile);
-		c.rm(destFile);
+		log.info("delete :{}",ftpFile);
+		c.rm(ftpFile);
 
 		log.info("disconnect");
 		c.disconnect();
