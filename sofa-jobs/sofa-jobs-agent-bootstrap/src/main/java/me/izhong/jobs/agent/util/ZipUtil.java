16b3cd0332986a01da1be3924a1481d1573bf5cb
jjzhong <jjzhong@chianums.com>
2020-01-08 11:33:05 +0800

    优化压缩工具类，增加编码,文件列表压缩

--- sofa-jobs/sofa-jobs-agent-bootstrap/src/main/java/me/izhong/jobs/agent/util/ZipUtil.java
+++ sofa-jobs/sofa-jobs-agent-bootstrap/src/main/java/me/izhong/jobs/agent/util/ZipUtil.java
@@ -1,19 +1,27 @@
 package me.izhong.jobs.agent.util;
 
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.StringUtils;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.InputStream;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Enumeration;
+import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
 import java.util.zip.ZipOutputStream;
 
+@Slf4j
 public class ZipUtil {
 
     private static final int DEFAULT_BUFFER_SIZE = 1024 * 8;
+    private static final Charset DEFAULT_CHARSET = Charset.defaultCharset();
 
     /**
      * 压缩文件，文件夹
@@ -22,13 +30,64 @@ public class ZipUtil {
      * @throws Exception
      */
     public static void zip(String zipFileName, File inputFile) throws Exception {
+        zip(zipFileName,inputFile,DEFAULT_CHARSET);
+    }
+
+    public static void zip(String zipFileName, File inputFile,Charset charset) throws Exception {
         if(StringUtils.isBlank(zipFileName))
             throw new Exception("要压缩的zip文件名称不能为空");
         if (inputFile == null || !inputFile.exists())
             throw new Exception("要压缩的文件不存在");
-        ZipOutputStream out = new ZipOutputStream(new FileOutputStream(zipFileName));
-        zip(out, inputFile, "");
-        out.close();
+        ZipOutputStream out = new ZipOutputStream(new FileOutputStream(zipFileName), charset);
+        try {
+            zip(out, inputFile, "");
+        } finally {
+            out.close();
+        }
+    }
+
+    /**
+     * 压缩文件 压缩后的文件在同一个目录
+     * @param zipFileName 压缩文件后的zip文件名称，全路径
+     * @param inputFiles 要压缩的文件列表
+     * @throws Exception
+     */
+    public static void zipFiles(String zipFileName, List<File> inputFiles) throws Exception {
+        zipFiles(zipFileName,inputFiles,DEFAULT_CHARSET);
+    }
+
+    public static void zipFiles(String zipFileName, List<File> inputFiles,Charset charset) throws Exception {
+        if (inputFiles == null || inputFiles.size() == 0)
+            throw new Exception("要压缩的文件不存在");
+        for (File f : inputFiles) {
+            if (!f.exists()) {
+                throw new Exception("文件" + f.getName() + "不存在");
+            }
+        }
+        ZipOutputStream out = new ZipOutputStream(new FileOutputStream(zipFileName),charset);
+        try {
+            for (File f : inputFiles) {
+                zip(out, f, f.getName());
+            }
+        } finally {
+            out.close();
+        }
+    }
+
+    public static void zipFiles(String zipFileName, String inputDir, List<String> inputFileNames) throws Exception {
+        zipFiles(zipFileName,inputDir,inputFileNames,DEFAULT_CHARSET);
+    }
+
+    public static void zipFiles(String zipFileName, String inputDir, List<String> inputFileNames, Charset charset) throws Exception {
+        if (inputFileNames == null || inputFileNames.size() == 0)
+            throw new Exception("要压缩的文件不存在");
+        if (inputDir != null && !inputDir.endsWith("/"))
+            inputDir += "/";
+        List<File> files = new ArrayList<>();
+        for(String fileName : inputFileNames){
+            files.add(new File(inputDir + fileName));
+        }
+        zipFiles(zipFileName,files,charset);
     }
 
     /**
@@ -39,29 +98,34 @@ public class ZipUtil {
      * @throws Exception
      */
     public static void zipFile(String zipFileName, File inputFile, String fileName) throws Exception {
+        zipFile(zipFileName,inputFile,fileName,DEFAULT_CHARSET);
+    }
+
+    public static void zipFile(String zipFileName, File inputFile, String fileName, Charset charset) throws Exception {
         if (inputFile == null || !inputFile.exists())
             throw new Exception("要压缩的文件不存在");
-        ZipOutputStream out = new ZipOutputStream(new FileOutputStream(zipFileName));
-        out.putNextEntry(new ZipEntry(fileName));
-        FileInputStream in = new FileInputStream(inputFile);
-        byte[] buf = new byte[DEFAULT_BUFFER_SIZE];
-        int b;
-        while ((b = in.read(buf)) != -1) {
-            out.write(buf, 0, b);
+        if(StringUtils.isBlank(fileName))
+            throw new Exception("要压缩的文件名称不能为空");
+        ZipOutputStream out = new ZipOutputStream(new FileOutputStream(zipFileName),charset);
+        try {
+            zip(out, inputFile, fileName);
+        } finally {
+            out.close();
         }
-        in.close();
-        out.close();
     }
 
     private static void zip(ZipOutputStream out, File srcFile, String base) throws Exception {
         if (srcFile.isDirectory()) {
             File[] fl = srcFile.listFiles();
-            out.putNextEntry(new ZipEntry(base + "/"));
+            if(StringUtils.isNotBlank(base))
+                out.putNextEntry(new ZipEntry(base + "/"));
             base = base.length() == 0 ? "" : base + "/";
             for (int i = 0; i < fl.length; i++) {
                 zip(out, fl[i], base + fl[i].getName());
             }
         } else {
+            if(StringUtils.isBlank(base))
+                throw new Exception("压缩异常");
             out.putNextEntry(new ZipEntry(base));
             FileInputStream in = new FileInputStream(srcFile);
             byte[] buf = new byte[DEFAULT_BUFFER_SIZE];
@@ -70,6 +134,8 @@ public class ZipUtil {
                 out.write(buf, 0, b);
             }
             in.close();
+            out.flush();
+            out.closeEntry();
         }
     }
 
@@ -80,6 +146,10 @@ public class ZipUtil {
      * @throws Exception
      */
     public static void unzip(String zipFileName, String path) throws Exception {
+        unzip(zipFileName,path,DEFAULT_CHARSET);
+    }
+
+    public static void unzip(String zipFileName, String path, Charset charset) throws Exception {
         if (zipFileName == null || !new File(zipFileName).exists())
             throw new Exception("要解压的文件不存在");
         if(StringUtils.isBlank(path))
@@ -87,7 +157,7 @@ public class ZipUtil {
         if(!path.endsWith(File.separator)) {
             path += File.separator;
         }
-        ZipFile zf = new ZipFile(new File(zipFileName));
+        ZipFile zf = new ZipFile(new File(zipFileName),charset);
         Enumeration<? extends ZipEntry> en = zf.entries();
         while (en.hasMoreElements()) {
             unzip(zf, en.nextElement(),path);
@@ -119,13 +189,58 @@ public class ZipUtil {
         }
     }
 
-
     public static void main(String[] temp) {
         try {
-            //zipFile("D:\\logs\\hello.zip", new File("D:\\logs\\zk.log\\tt.txt"),"xx.tt");
-            zip("D:\\logs\\hello.zip", new File("D:\\logs\\zk.log"));
-            System.out.println("压缩完成"); // 输出信息
-            unzip("D:\\logs\\hello.zip","D:\\logs\\ppp");
+            String tmpDir = File.createTempFile("zip","tmp").getParent();
+            log.info("设置临时工作目录:{}",tmpDir);
+            String sourceDir = tmpDir + File.separator + "source";
+            new File(sourceDir).mkdir();
+            log.info("设置源目录:{}",sourceDir);
+            String destDir = tmpDir + File.separator + "dest";
+            new File(destDir).mkdir();
+            log.info("设置目标目录:{}",destDir);
+
+            File sourceFile1 = new File(sourceDir + File.separator + "测试文件1.txt");
+            sourceFile1.createNewFile();
+            FileUtils.write(sourceFile1,"content1内容", DEFAULT_CHARSET);
+            File sourceFile2 = new File(sourceDir + File.separator + "测试文件2.txt");
+            sourceFile2.createNewFile();
+            FileUtils.write(sourceFile2,"content2内容", DEFAULT_CHARSET);
+
+            File sourceDir1 = new File(sourceDir + File.separator + "测试目录");
+            sourceDir1.mkdirs();
+            File sourceDirFile1 = new File(sourceDir1.getAbsolutePath() + File.separator + "测试子文件.txt");
+            sourceDirFile1.createNewFile();
+
+            log.info("列出源文件夹文件");
+            for(File f: new File(sourceDir).listFiles()){
+                if(f.isDirectory()) {
+                    log.info("目录:{}" , f.getName());
+                    for(File f2: f.listFiles()){
+                        log.info("文件:{}/{}",f.getName(),f2.getName());
+                    }
+                } else {
+                    log.info("文件:{}",f.getName());
+                }
+            }
+
+            //压缩文件
+            zip(destDir + File.separator + "压缩后的文件夹.zip",new File(sourceDir));
+            log.info("压缩文件成功:{}",destDir + File.separator + "压缩后的文件夹.zip");
+
+            //如果是utf8编码 fileName 不能含有 自定义，否则win自带的解压缩工具显示不了zip里面文件名字
+            zipFile(destDir + File.separator + "压缩单个文件.zip",sourceFile1,"测试文件1444自定义.txt");
+
+            zipFiles(destDir + File.separator + "压缩后的文件列表1.zip", Arrays.asList(new File[]{sourceFile1,sourceFile2}));
+
+            zipFiles(destDir + File.separator + "压缩后的文件列表2.zip",sourceDir, Arrays.asList(new String[]{sourceFile1.getName(),sourceFile2.getName()}));
+
+            log.info("压缩成功");
+            File unzipDir = new File(destDir +File.separator + "unzip");
+            if(!unzipDir.exists())
+                unzipDir.mkdir();
+            unzip(destDir + File.separator + "压缩后的文件夹.zip",unzipDir.getAbsolutePath());
+            log.info("解压成功");
         } catch (Exception ex) {
             ex.printStackTrace();
         }
